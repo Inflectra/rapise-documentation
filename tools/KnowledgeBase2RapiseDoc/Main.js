b342776f1afa0beff6a2dc62ac0f75c0678b691b
Denis Markovtsev <denis@inflectra.com>
2024-07-26 17:04:11 +0300

    Adjusted to load Spira KBs as well (product selection)

--- tools/KnowledgeBase2RapiseDoc/Main.js
+++ tools/KnowledgeBase2RapiseDoc/Main.js
@@ -2,10 +2,8 @@
 
 function Test(params)
 {
-
-
 	RVL.DoPlayScript("Main.rvl.xlsx", "RVL");
 }
 
-g_load_libraries=["%g_browserLibrary%"]
+g_load_libraries=["Web"]
 
--- tools/KnowledgeBase2RapiseDoc/User.js
+++ tools/KnowledgeBase2RapiseDoc/User.js
@@ -5,3 +5,53 @@ function SaveDomTree()
 	var dom = Navigator.GetDomTree();
 	Navigator.SaveDomToXml("List.xml", dom);
 }
+
+var tfn = "TopicIDs.txt";
+function SaveTopicIDs(/**string*/ product)
+{
+	tfn = "TopicIDs_" + product + ".txt";
+	var links = Navigator.DOMFindByXPath("//a[contains(@href,\"/Support/KnowledgeBase/KB\") and contains(@href,\".aspx\")]", true);
+	File.Write(tfn, "");
+	for(var i = 0; i < links.length; i++)
+	{
+		var link = links[i].GetHref();
+		link = link.replace("https://www.inflectra.com/Support/KnowledgeBase/", "").replace(".aspx", "");
+		File.Append(tfn, link + "\r\n");
+	}
+}
+
+function ProcessTopics(/**string*/ product)
+{
+	tfn = "TopicIDs_" + product + ".txt";
+	var ids = File.Read(tfn).split("\r\n");
+	for(var i = 0; i < ids.length; i++)
+	{
+		var id = ids[i].trim();
+		if (id)
+		{
+			Log(id);
+			var topicFile = Global.GetFullPath("Topics_" + product + "\\"+ id + ".md");
+			if (!File.Exists(topicFile))
+			{
+				global.Topic = id;
+				RVL.DoPlaySheet("LoadArticle");
+				//break;
+			}
+		}
+	}
+}
+
+function SaveTopic(/**string*/ fileName, /**string*/ title, /**string*/ description, /**string*/ body)
+{
+	File.Write(fileName, "# Title" + "\r\n\r\n");
+	File.Append(fileName, title + "\r\n\r\n");
+	File.Append(fileName, "## Description" + "\r\n\r\n");
+	File.Append(fileName, description + "\r\n\r\n");
+	File.Append(fileName, "## Body" + "\r\n\r\n");
+	File.Append(fileName, body + "\r\n\r\n");
+}
+
+function GetInnerHtml(/**object*/ obj)
+{
+	return WebDriver.ExecuteScript('return arguments[0].innerHTML;', obj.element.e);
+}
