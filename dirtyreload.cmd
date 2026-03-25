af0a8dee15a4e579b4a4240f5b72256fbf5e2710
Alexey Grinevich <alexey.grinevich@inflectra.com>
2026-03-25 17:07:01 +0300

    call to fix the .cmd

--- dirtyreload.cmd
+++ dirtyreload.cmd
@@ -4,9 +4,7 @@ rem echo Calling mkdocs serve
 
 pushd %~dp0\tools
 
-python mkdocsyml.py --filter "hide_site_name:" "prebuild_index: True"
-
-echo "AAAAAAAA"
+call python mkdocsyml.py --filter "hide_site_name:" "prebuild_index: True"
 
 popd
 
